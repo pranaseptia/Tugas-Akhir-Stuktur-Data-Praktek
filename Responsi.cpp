ffac8cdf06e49162a5415f2235ae0140bf81c2ca
Prana Septia <43823516+prnz98@users.noreply.github.com>
2020-01-02 22:05:36 +0700

    5170411184 Menambahkan Struct
    pada kesempatan ini saya menambahkan deklarasi struct yaitu data mobil, data driver, data konsumen, tanggal, data transaksi. data pembayaran

--- Responsi.cpp
+++ Responsi.cpp
@@ -1,1 +1,82 @@
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+#include <math.h>
 
+#define DRIVER 8
+#define MAX 14
+
+using namespace std;
+
+typedef string infotype;
+//Data Mobil
+typedef struct tNode *address;
+typedef bool status;
+typedef struct tNode{
+	infotype NoTNKB;
+	infotype Merk;
+	infotype Warna;
+	infotype Tahun;
+	int biayaSewa;
+	status sewa;
+	address Next;
+}Mobil;
+typedef Mobil *List;
+
+//Data Driver
+typedef int pos;
+typedef struct{
+	pos Head;
+	pos Tail;
+	infotype noKTP[DRIVER];
+	infotype Nama[DRIVER];
+	infotype Alamat[DRIVER];
+	infotype Usia[DRIVER];
+	status service[DRIVER];
+}Queue;
+
+//Data Konsumen
+typedef struct{
+	pos Head;
+	pos Tail;
+	infotype noKTP[MAX];
+	infotype Nama[MAX];
+	infotype Alamat[MAX];
+}Konsumen;
+
+//Tanggal
+typedef struct{
+	int Tanggal;
+	int Bulan;
+	int Tahun;
+}Date;
+
+//Data Transaksi
+typedef struct TNode *ID_Transaksi;
+typedef struct TNode{
+	infotype Nama_Konsumen;
+	infotype Alamat;
+	infotype NoTNKB;
+	infotype Nama_Driver;
+	Date Sewa;
+	Date Kembali;
+	int biayaSewa;
+	ID_Transaksi Next;
+}transaksi;
+typedef transaksi *Transaksi;
+
+//Data Pembayaran
+typedef struct TBayar *ID_Pembayaran;
+typedef struct TBayar{
+	infotype Nama_Konsumen;
+	infotype Alamat;
+	infotype NoTNKB;
+	infotype Nama_Driver;
+	Date Sewa;
+	Date Kembali;	
+	Date Bayar;
+	int Denda;
+	int totBiaya;
+	ID_Pembayaran Next;
+}Pembayaran;
+typedef Pembayaran *pembayaran;
